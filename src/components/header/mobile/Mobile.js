bc50877ba50bc61a723918e0d294675e1f6ab09b
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-04-30 18:22:50 +0200

    add header's mobile content

--- src/components/header/mobile/Mobile.js
+++ src/components/header/mobile/Mobile.js
@@ -1,7 +1,45 @@
 import React from "react";
+import "./Moblie.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsCodeSlash } from "react-icons/bs";
+import { MdWorkOutline } from "react-icons/md";
+import { FiUser } from "react-icons/fi";
 
-const Mobile = () => {
-  return <div>Mobile</div>;
+const Mobile = ({ isOpen, setIsOpen }) => {
+  return (
+    <div className="mobile">
+      <div className="close-icon" onClick={() => setIsOpen(!isOpen)}>
+        <AiOutlineCloseCircle />
+      </div>
+      <div className="mobile-options">
+        <div className="mobile-option">
+          <a href="#projects">
+            <FaLaptopCode className="option-icon" />
+            projects
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#skills">
+            <BsCodeSlash className="option-icon" />
+            skills
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#work">
+            <MdWorkOutline className="option-icon" />
+            work
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#contact">
+            <FiUser className="option-icon" />
+            contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mobile;
