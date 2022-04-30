2fa7d92c59c8d5263a0959338f931322a6f341a9
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-04-30 17:21:12 +0200

    add responsive css to the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Header.css";
+import Mobile from "./mobile/Mobile";
+import Web from "./web/Web";
+import { AiOutlineAppstore } from "react-icons/ai";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="logo">Osama Salah</div>
+      <div className="menu">
+        <div className="web-menu">
+          <Web />
+        </div>
+        <div className="mobile-menu">
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <AiOutlineAppstore className="menu-icon" />
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
