e0e3b87dc5273891004309dbaa5653ae7d8e104c
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-04-30 18:03:03 +0200

    add header's web content

--- src/components/header/web/Web.js
+++ src/components/header/web/Web.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Web.css";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsCodeSlash } from "react-icons/bs";
+import { MdWorkOutline } from "react-icons/md";
+import { FiUser } from "react-icons/fi";
 
 const Web = () => {
-  return <div>Web</div>;
+  return (
+    <div className="web">
+      <div className="web-option">
+        <a href="#projects">
+          <FaLaptopCode className="option-icon" />
+          projects
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#skills">
+          <BsCodeSlash className="option-icon" />
+          skills
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#work">
+          <MdWorkOutline className="option-icon" />
+          work
+        </a>
+      </div>
+      <div className="web-option">
+        <a href="#contact">
+          <FiUser className="option-icon" />
+          contact
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Web;
