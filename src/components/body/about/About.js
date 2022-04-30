cfe99d0929dd61cb4496a92f5f26973df2e989b6
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-04-30 19:21:42 +0200

    add info and image to about section

--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          Hello There 👋, I am
+          <br /> <span className="info-name">Osama Salah</span>.
+          <br /> I love experimenting with th web.
+        </div>
+        <div className="about-img">
+          <img src={require("./coding.png")} className="picture" />
+        </div>
+      </div>
+      <div className="about-bottom"></div>
+    </div>
+  );
 };
 
 export default About;
