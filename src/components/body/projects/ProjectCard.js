5914ef885bc2f9026660a38f2444b2ee8274b1da
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 15:08:57 +0200

    finish project card

--- src/components/body/projects/ProjectCard.js
+++ src/components/body/projects/ProjectCard.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./ProjectCard.css";
-const ProjectCard = (project) => {
+import { AiOutlineGlobal, AiFillGithub } from "react-icons/ai";
+const ProjectCard = ({ project }) => {
   return (
     <div className="project-card">
-      <div className="project-info"></div>
+      <div className="project-info">
+        <label className="project-title">{project.title}</label>
+        <div className="project-links">
+          {project.demo && (
+            <a className="project-link" href={project.demo}>
+              <div className="link-button">
+                <AiOutlineGlobal />
+                Demo
+              </div>
+            </a>
+          )}
+          {project.github && (
+            <a className="project-link" href={project.github}>
+              <div className="link-button">
+                <AiFillGithub />
+                Github
+              </div>
+            </a>
+          )}
+        </div>
+        <p>{project.about}</p>
+        <div className="project-tags">
+          {project.tags.map((tag) => {
+            return <label className="tag">{tag}</label>;
+          })}
+        </div>
+      </div>
       <img src={project.image} className="project-photo"></img>
     </div>
   );
