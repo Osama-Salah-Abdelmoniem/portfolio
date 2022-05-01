0be03d038958d74e3cece54f4e11be54609350db
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 14:34:33 +0200

    add project Card to projects section

--- src/components/body/projects/Projects.js
+++ src/components/body/projects/Projects.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { ProjectData } from "../../data/projects";
+import ProjectCard from "./ProjectCard";
+import "./Projects.css";
 const Projects = () => {
-  return <div>Projects</div>;
+  const data = ProjectData;
+  return (
+    <div className="projects">
+      <label className="section-title">Projects</label>
+      <div>
+        {data.map((project) => {
+          return <ProjectCard project={project} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
