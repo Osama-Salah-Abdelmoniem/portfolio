74eca915067cbd60677c0d23d68c114d0b202939
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 16:59:55 +0200

    add ityped

--- src/components/body/about/About.js
+++ src/components/body/about/About.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SocialContact from "../../common/socialcontact/SocialContact";
 import "./About.css";
+import { init } from "ityped";
 
 const About = () => {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Back-end Developer", "MERN Stack Developer"],
+    });
+  }, []);
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
           Hello There 👋, I am
           <br /> <span className="info-name">Osama Salah</span>.
-          <br /> I love experimenting with th web.
+          <br /> <span ref={textRef}></span>
         </div>
         <div className="about-img">
           <img src={require("./coding.png")} className="picture" />
