0dbd2b99f086387c0f6d8f2d4476f9255918c781
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 16:03:04 +0200

    add skills data

---
+++ src/components/data/skills.js
@@ -0,0 +1,62 @@
+export const SkillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "HTML",
+        icon: <i class="devicon-html5-plain colored"></i>,
+      },
+      {
+        name: "CSS",
+        icon: <i class="devicon-css3-plain colored"></i>,
+      },
+      {
+        name: "JavaScript",
+        icon: <i class="devicon-javascript-plain colored"></i>,
+      },
+      {
+        name: "ReactJs",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Backend",
+    list: [
+      {
+        name: "NodeJs",
+        icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Linux",
+    list: [
+      {
+        name: "linux",
+        icon: <i class="devicon-linux-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "Programming Languages",
+    list: [
+      {
+        name: "C",
+        icon: <i class="devicon-c-plain colored"></i>,
+      },
+      {
+        name: "C++",
+        icon: <i class="devicon-cplusplus-plain colored"></i>,
+      },
+      {
+        name: "Java",
+        icon: <i class="devicon-java-plain colored"></i>,
+      },
+      {
+        name: "Python",
+        icon: <i class="devicon-python-plain colored"></i>,
+      },
+    ],
+  },
+];
