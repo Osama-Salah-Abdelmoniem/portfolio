8d7ce5099e18eba2f7505a8d5aa11b98a6892c72
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 16:30:40 +0200

    finish contact section

--- src/components/body/contact/Contact.js
+++ src/components/body/contact/Contact.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import "./Contact.css";
+import Separator from "../../common/separator/Separator";
+import SocialContact from "../../common/socialcontact/SocialContact";
+import { FiDownload } from "react-icons/fi";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <Separator />
+      <label className="section-title">Contact</label>
+      <div className="contact-container">
+        <div className="contact-left">
+          <p>Get in touch! Contact me on my any of the platforms</p>
+          <SocialContact />
+        </div>
+        <div className="download">
+          <a download href={require("../../data/Osama_Salah_Resume.pdf")}>
+            <FiDownload className="download-icon" />
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
