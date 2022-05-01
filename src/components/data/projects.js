be034ba1ca4f93749827c5e06051dbadb7c32f2e
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-01 14:09:22 +0200

    add projects object

---
+++ src/components/data/projects.js
@@ -0,0 +1,12 @@
+export const ProjectData = [
+  {
+    id: 1,
+    title: "Portfolio Project",
+    about:
+      "This is a Portfolio project that represents my prjects, skills,work and contacts",
+    tags: ["ReactJs"],
+    demo: "www.link",
+    github: "www.githubdfgdf",
+    image: require("./Project-Forecast.png"),
+  },
+];
