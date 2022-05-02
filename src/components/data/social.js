9e7e13a708ee91c2e2fd86ccf28b118160f39244
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-02 18:29:04 +0200

    add social links

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,17 +1,17 @@
 export const SocialData = [
   {
     platform: "Github",
-    link: "www.github.com/Osama-Salah-Abdelmoniem",
+    link: "https://github.com/Osama-Salah-Abdelmoniem",
     icon: require("./github(1).png"),
   },
   {
     platform: "Linkedin",
-    link: "www.linkedin.com/in/osamasalah99/",
+    link: "https://linkedin.com/in/osamasalah99/",
     icon: require("./linkedin(1).png"),
   },
   {
     platform: "Twitter",
-    link: "www.twitter.com/0xKato",
+    link: "https://twitter.com/0xKato",
     icon: require("./twitter(2).png"),
   },
 ];
