2ffced76947b8d4a61d415aa4c030d18e74458f8
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-02 23:47:33 +0200

    finish projects section

--- src/components/body/projects/Projects.js
+++ src/components/body/projects/Projects.js
@@ -8,12 +8,17 @@ const Projects = () => {
   return (
     <div className="projects">
       <Separator />
+      <br />
       <label className="section-title">Projects</label>
       <div>
         {data.map((project) => {
           return <ProjectCard project={project} />;
         })}
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
