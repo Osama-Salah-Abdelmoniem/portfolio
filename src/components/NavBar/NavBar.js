8ebe52ea0b367b58001bc56ae6e21e2159022436
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-02 17:55:20 +0200

    add new navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,80 @@
+import { Link } from "react-scroll";
+import { useState } from "react";
+import "./NavBar.css";
+import { FaLaptopCode } from "react-icons/fa";
+import { BsCodeSlash } from "react-icons/bs";
+import { CgUserlane } from "react-icons/cg";
+import { FiUser } from "react-icons/fi";
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
+
+  return (
+    <div>
+      <div className={click ? "main-container" : ""} onClick={() => Close()} />
+      <header className="navbar" onClick={(e) => e.stopPropagation()}>
+        <div className="nav-container">
+          <Link exact to="/" className="nav-logo">
+            OS.
+          </Link>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                spy
+                to="about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                <CgUserlane />
+                about
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                spy
+                to="projects"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                <FaLaptopCode />
+                projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                spy
+                to="skills"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                <BsCodeSlash />
+                skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                spy
+                to="contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={click ? handleClick : null}
+              >
+                <FiUser />
+                contact
+              </Link>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+export default NavBar;
