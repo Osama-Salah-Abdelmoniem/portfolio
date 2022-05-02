6b650c985bb95a07f5916ec791e7ca4abc57c73d
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-03 00:36:18 +0200

    add project describtion

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,10 +3,10 @@ export const ProjectData = [
     id: 1,
     title: "Portfolio Project",
     about:
-      "This is a Portfolio project that represents my prjects, skills,work and contacts",
+      "This is a Portfolio project that represents my projects, skills, and contacts",
     tags: ["ReactJs"],
     demo: "www.link",
-    github: "www.githubdfgdf",
+    github: "www.https://github.com/Osama-Salah-Abdelmoniem/portfolio",
     image: require("./p1.png"),
   },
 ];
