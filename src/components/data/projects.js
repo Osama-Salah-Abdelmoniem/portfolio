cb5907776d4a189718f4607d7136416addcbc0a8
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 05:52:55 +0200

    add feedback project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,8 +5,17 @@ export const ProjectData = [
     about:
       "This is a Portfolio project that represents my projects, skills, and contacts",
     tags: ["ReactJs"],
-    demo: "www.link",
-    github: "www.https://github.com/Osama-Salah-Abdelmoniem/portfolio",
+    demo: "https://osamasalahportfolio.netlify.app/",
+    github: "https://github.com/Osama-Salah-Abdelmoniem/portfolio",
     image: require("./prot.png"),
+  },
+  {
+    id: 2,
+    title: "Feedback app",
+    about: "This is a React app to leave feedback for a product or service",
+    tags: ["ReactJs"],
+    demo: "https://feedback-ui-application.netlify.app/",
+    github: "https://github.com/Osama-Salah-Abdelmoniem/Feedback-App",
+    image: require("./Feedback-app.png"),
   },
 ];
